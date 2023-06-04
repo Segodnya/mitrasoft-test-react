424b6052cd3ac4eef105fca02153419a262c80b3
Kirill Kurentsov <immortalsteel@gmail.com>
2023-06-04 13:37:37 +0300

    Добавлен HashRouter для работы с Github Pages

--- src/index.js
+++ src/index.js
@@ -4,15 +4,15 @@ import { Provider } from "react-redux";
 import store from "./redux/store";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./App";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOM.createRoot(rootElement);
 
 root.render(
-  <BrowserRouter>
+  <HashRouter>
     <Provider store={store}>
       <App />
     </Provider>
-  </BrowserRouter>
+  </HashRouter>
 );
